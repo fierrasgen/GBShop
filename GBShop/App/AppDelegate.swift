6aa785d59d0e97fe92e17bceb4e8d2034db846c0
Fierrasgen <Look00@inbox.ru>
2021-07-05 20:10:47 +0300

    Fix after review

--- GBShop/App/AppDelegate.swift
+++ GBShop/App/AppDelegate.swift
@@ -9,24 +9,24 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     let requestFactory = RequestFactory()
     var token: String?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let auth = requestFactory.makeAuthRequestFatory()
-            auth.login(userName: "Somebody", password: "mypassword") { response in
-                switch response.result {
-                case .success(let login):
-                    print(login)
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
+        auth.login(userName: "Somebody", password: "mypassword") { response in
+            switch response.result {
+            case .success(let login):
+                print(login)
+            case .failure(let error):
+                print(error.localizedDescription)
             }
-            return true
+        }
+        return true
     }
-   
+    
     func logoutResult() {
         guard let token = self.token else { return }
         
@@ -66,21 +66,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
-
-
+    
+    
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
 
